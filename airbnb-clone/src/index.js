085c60108e86e393e7cff64c76f9e9aac55f0e0e
ac <alcordani@gmail.com>
2017-10-29 13:49:07 +0100

    first component

--- airbnb-clone/src/index.js
+++ airbnb-clone/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Hello from "./components/hello";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));
 registerServiceWorker();
