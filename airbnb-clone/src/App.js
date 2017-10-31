60c821005337a51bb3e1556473dde61b1e6aeea8
ac <alcordani@gmail.com>
2017-10-31 19:29:00 +0100

    state

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-import Hello from "./components/hello";
+import Flat from "./components/flat";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+      this.setState({
+        flats: data
+
+      });
+    })
+  }
   render() {
+  	
+
     return (
-      <Hello />
+      
+      	<div className="app">
+          <div className="main">
+            <div className="search">
+            </div>
+            <div className="flats">
+              {this.state.flats.map((flat)  => {
+                return <Flat key={flat.name} flat={flat} />
+              })}
+            </div>
+          </div>
+          <div className="map">
+          </div>
+        </div>
+
+      
     );
   }
 }
--- airbnb-clone/src/index.js
+++ airbnb-clone/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from "./components/hello";
+// import Hello from "./components/hello";
 
-const root = 
-	<div>
-		<Hello firstName="Alex" lastName="Cordani" />
-		<Hello firstName="Alex" lastName="Cordani" />
-	</div>
+// const root = 
+// 	<div>
+// 		<Hello firstName="Alex" lastName="Cordani" />
+// 		<Hello firstName="Alex" lastName="Cordani" />
+// 	</div>
 
-ReactDOM.render(root, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
